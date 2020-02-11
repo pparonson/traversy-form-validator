d9caf122f5c3e4b193c784d5c241ad0aca095ab7
paronson <paronson@qw-corp.com>
2020-02-11 16:41:46 -0700

    adding styles

--- src/index.js
+++ src/index.js
@@ -14,15 +14,5 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static("public"));
 
-// app.post("/ajax", (req, res) => {
-//   // console.log(`ajax: ${JSON.stringify(req, null, 2)}`)
-//   console.log(req.body);
-//   // res.send sends content-type: text/html as default
-//   // res.send("<h2>Test Page</h2>")
-
-//   // changes response to content-type: application/json
-//   res.json("<h2>Test Page</h2>")
-// })
-
 const PORT = 3000;
 app.listen(PORT, () => console.log(`Server is listening on port: ${PORT}`));
